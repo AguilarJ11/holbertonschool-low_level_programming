1056e29ca88f1a59dabbcc644f71de9e3c897044
AguilarJ11 <8664@holbertonstudents.com>
2024-02-15 07:14:23 -0800

    commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Esto es un comentario
+ * Return: Esto es un comentario
+ */
+int main(void)
+{
+	char a;
+	int b;
+	float c;
+	long int d;
+	long long e;
+
+printf("Size of a char: %d\n", (a));
+printf("Size of an int: %d\n", (b));
+printf("Size of a long int: %ld\n", (d));
+printf("Size of a long long: %lld\n", (e));
+printf("Size of a float: %f\n", (c));
+
+	return (0);
+
+}
