e72bd34de7d8bc44a0bf8051694bf9498f8cee8e
AguilarJ11 <8664@holbertonstudents.com>
2024-02-15 07:59:59 -0800

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 	int b;
 	float c;
 	long int d;
-	long long e;
+	long long int e;
 
 printf("Size of a char: %lu byte\n", sizeof(a));
 printf("Size of an int: %lu byte\n", sizeof(b));
 printf("Size of a long int: %lu byte\n", sizeof(d));
-printf("Size of a long long: %lu byte\n", sizeof(d));
+printf("Size of a long long int: %lu byte\n", sizeof(d));
 printf("Size of a float: %lu byte\n", sizeof(e));
 
 	return (0);
