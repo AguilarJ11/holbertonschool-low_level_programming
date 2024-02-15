f860e7368f104bc2917d8e1de6015918f57d275f
AguilarJ11 <8664@holbertonstudents.com>
2024-02-15 06:26:39 -0800

    commit

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Esto es un comentario
+* Return: Esto es un comentario
+*/
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
