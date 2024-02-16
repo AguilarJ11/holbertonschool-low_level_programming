79ea08556bf87ce37049e52f51d50f65d133aa44
AguilarJ11 <8664@holbertonstudents.com>
2024-02-16 18:20:01 +0000

    commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - descripcion
+ * Return: descripcion
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
