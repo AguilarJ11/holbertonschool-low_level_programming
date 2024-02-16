59a1d6b7a54236c86c0313c3756bf45b58fdc735
AguilarJ11 <8664@holbertonstudents.com>
2024-02-16 20:17:52 +0000

    commit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - description
+ *Return: chau
+ */
+
+int main(void)
+
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+	putchar(a);
+
+	a--;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
