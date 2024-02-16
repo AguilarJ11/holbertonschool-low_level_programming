bc96f8d94679b6a83e051511ccc0e0f3f6af96e4
AguilarJ11 <8664@holbertonstudents.com>
2024-02-16 23:33:56 +0000

    commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,15 +6,13 @@
  *@c: description
  */
 
-int _putchar(char c);
+int _putchar(char);
 
 int main(void)
 {
 	char c = 0;
-	char d = 0;
 
-	c =_putchar(d);
-	c--;
+	 _putchar(c);
 
-	return (c);
+	return (0);
 }
