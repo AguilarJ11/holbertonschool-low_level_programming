ed0a96880b524988cee5eeb0faaf09afb5370e43
AguilarJ11 <8664@holbertonstudents.com>
2024-02-16 20:49:44 +0000

    commit

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - descripcion
+ * Return: descripcion
+ */
+
+int main(void)
+{
+
+	int a = 0;
+
+	while (a <= 9)
+	{
+
+	putchar(a + '0');
+
+	putchar(',');
+
+	putchar(' ');
+
+	a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
