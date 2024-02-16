4b83410ac44504d21011025b67df9ee77222a013
AguilarJ11 <8664@holbertonstudents.com>
2024-02-16 18:02:39 +0000

    commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - descripcion
+ * Return: descripcion
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
