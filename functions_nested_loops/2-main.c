9dacb45fed805a20dd6dd9692c2ba13691ab5864
AguilarJ11 <8664@holbertonstudens.com>
2024-02-19 14:11:10 +0000

    commit

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - description
+ * Return: description
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return(0);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ *print_alphabet_x10 - descripcion
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 'a';
+	int b = 0;
+
+	while (b <= 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		b++;
+	}
+
+	_putchar('\n');
+}
