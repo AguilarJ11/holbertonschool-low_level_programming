f0d22d37649fb87726088f32540d2a01b03be0af
AguilarJ11 <8664@holbertonstudents.com>
2024-02-19 21:37:06 +0000

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,34 +1,39 @@
 #include "main.h"
 /**
- *
- *
+ *jack_bauer - print every minute and hour of the day
  */
 void jack_bauer(void)
 {
 	int h;
 	int m;
 
-	while ((h <= 23) && (m <= 59))
+	for (h = 0; h <= 23; h++)
 	{
-		for (m = 0; m < 59; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			if (h < 10)
 			{
-				_putchar('0');
+				_putchar(48);
+				_putchar(h + 48);
 			}
 			else
 			{
-				_putchar(h / 10);
+				_putchar(h + 48);
 			}
-		}
-		for (h = 0; h < 23; h++)
-		{
-			if ()
+
+			_putchar(':');
+
+			if (m < 10)
 			{
+				_putchar(48);
+				_putchar(m + 48);
 			}
 			else
 			{
+				_putchar(m + 48);
 			}
+
+			_putchar('\n');
 		}
 	}
 }
