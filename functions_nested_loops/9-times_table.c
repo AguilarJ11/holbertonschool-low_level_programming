e808b797998d72ad667f65498f0fd71e76180257
AguilarJ11 <8664@holbertonstudents.com>
2024-02-19 23:40:14 +0000

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(r / 10 + 48);
 				_putchar(r % 10 + 48);
 			}
-			if (n != 9 && n * t  <= 9)
+			if (n != 9 && r + t <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
