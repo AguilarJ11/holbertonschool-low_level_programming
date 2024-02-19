2b0dc3fb27da80d6aa81badac617dac9168dbd51
AguilarJ11 <8664@holbertonstudens.com>
2024-02-19 13:53:38 +0000

    commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+#endif
+
+/**
+ *print_alphabet - descripcion
+ */
+
+void print_alphabet(void)
+{
+	int a = 'a';
+
+	while (a <= 'z')
+	{
+
+	_putchar(a);
+
+	}
+
+	_putchar('\n');
+}
