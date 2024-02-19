308147e85affd122490208c83b9ad6e66327a03a
AguilarJ11 <8664@holbertonstudens.com>
2024-02-19 15:55:30 +0000

    commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - description
+ *@a: description
+ *Return: description
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
