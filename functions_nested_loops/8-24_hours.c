92472993efaa35c89cb0c24b9ed63abbb0b2119f
AguilarJ11 <8664@holbertonstudens.com>
2024-02-19 18:19:56 +0000

    commit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	while ((h <= 23) && (m <= 59))
+	{
+		for (m = 0; m < 59; m++)
+		{
+			if (h < 10)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(h / 10);
+			}
+		}
+		for (h = 0; h < 23; h++)
+		{
+			if ()
+			{
+			}
+			else
+			{
+			}
+		}
+	}
+}
