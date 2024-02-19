1d8c3c4d6c5df433f50190f7d4a4710b4597465b
AguilarJ11 <8664@holbertonstudens.com>
2024-02-19 15:04:24 +0000

    commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *print_sign - description
+ *@n: description
+ *Return: description
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+	_putchar('\n');
+}
