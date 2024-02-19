311cb641b8d2df050e9997a9216fbfec4c8144dd
AguilarJ11 <8664@holbertonstudents.com>
2024-02-19 22:59:24 +0000

    commit

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *time_table - description
+ */
+
+void times_table(void)
+{
+	int n, r, t;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			r = n * t;
+			if (r < 10)
+			{
+				_putchar(r + 48);
+			}
+			else
+			{
+				_putchar(r / 10 + 48);
+				_putchar(r % 10 + 48);
+			}
+			if (n != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
