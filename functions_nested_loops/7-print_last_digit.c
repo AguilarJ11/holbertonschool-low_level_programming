028909618f098c1c99b622c7b8d1e146be94f665
AguilarJ11 <8664@holbertonstudens.com>
2024-02-19 16:12:54 +0000

    commit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_last_digit - description
+ *@a: description
+ *@b: description
+ *Return: description
+ */
+
+int print_last_digit(int a)
+{
+	int b = a % 10;
+	return (b);
+}
+
