25a402024820dc1e843a5859ad551dd0f6242b84
AguilarJ11 <8664@holbertonstudents.com>
2024-02-19 21:48:51 +0000

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,24 +12,24 @@ void jack_bauer(void)
 		{
 			if (h < 10)
 			{
-				_putchar('0');
-				_putchar(h + '0');
+				_putchar(48);
+				_putchar(h + 48);
 			}
 			else
 			{
-				_putchar(h +'0');
+				_putchar(h);
 			}
 
 			_putchar(':');
 
 			if (m < 10)
 			{
-				_putchar('0');
-				_putchar(m + '0');
+				_putchar(48);
+				_putchar(m + 48);
 			}
 			else
 			{
-				_putchar(m + '0');
+				_putchar(m);
 			}
 
 			_putchar('\n');
