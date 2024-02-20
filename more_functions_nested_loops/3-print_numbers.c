2e21efa1f69da740bc9ce1ed71e9263ec15596db
AguilarJ11 <8664@holbertonstudents.com>
2024-02-20 15:50:43 +0000

    commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - description
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a < 9)
+	{
+		_putchar(a + 48);
+		_putchar('\n');
+		a++;
+	}
+}
