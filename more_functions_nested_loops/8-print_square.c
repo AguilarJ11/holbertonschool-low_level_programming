85d246781ecec041551312064e9188873ba02bf9
AguilarJ11 <8664@holbertonstudents.com>
2024-02-20 21:51:22 +0000

    commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_square - description
+ *@n: description
+ */
+
+void print_square(int n)
+{
+	int t, s;
+
+	for (t = 0; n > t; t++)
+	{
+		for (s = 0; n > s; s++)
+		{
+			_putchar('#'):
+		}
+
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+	_putchar('\n');
+
+}
+
