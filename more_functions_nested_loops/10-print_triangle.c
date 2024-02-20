2a079dbc49c00ebe5dce57adcf0e60f2d4ed398b
AguilarJ11 <8664@holbertonstudents.com>
2024-02-20 22:46:40 +0000

    commit

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_triangle - description
+ *@n: description
+ */
+
+void print_triangle(int n)
+{
+	int t, s;
+
+	for (t = 0; n > t; t++)
+	{
+		for (s = 0; n > s; s++)
+		{
+			if (n != s)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
