15296b80ae4dd205e0c5b8f2b8b72a38f31c52e4
AguilarJ11 <8664@holbertonstudents.com>
2024-02-20 21:18:56 +0000

    commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line - description
+ *@n: description
+ */
+
+void print_line(int n)
+{
+	int t;
+
+	for (t = 0; n > t; t++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
