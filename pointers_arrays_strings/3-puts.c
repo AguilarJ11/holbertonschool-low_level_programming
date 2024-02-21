acfc96d899a0f1ebe3ea11b6160919b679b46457
AguilarJ11 <8664@holbertonstudents.com>
2024-02-21 20:48:35 +0000

    commit

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
+ *_puts - stdout a string and a new line
+ *@str: entry string
  */
 
 void _puts(char *str)
 {
-	int a;
+	int a = 0;
 
 	while (str[a] != '\0')
 	{
