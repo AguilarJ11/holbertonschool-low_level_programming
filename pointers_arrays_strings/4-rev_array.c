efcfbc2e6587a3caa278edbc80c67b5a9b00fa74
AguilarJ11 <8664@holbertonstudents.com>
2024-02-23 22:01:51 +0000

    commit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverse an array of ints
+ *@a: entry array
+ *@n: number of elements of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int c, half, tmp;
+
+	if (n % 2 == 0)
+		half = n / 2;
+	else
+		half = (n - 1) / 2;
+
+	for (c = 0; c < half; c++)
+	{
+		tmp = a[c];
+		a[c] = a[n - 1 - c);
+		a[n - 1 - c] = tmp
+	}
+}
+
