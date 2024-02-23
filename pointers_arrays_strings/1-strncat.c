b2fa2b8a86feb7c94ae83f789d606cd53c2169dd
AguilarJ11 <8664@holbertonstudents.com>
2024-02-23 19:20:30 +0000

    commit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenate 2 strings but only n bytes from the second string
+ *@dest: string destination
+ *@src: second string
+ *@n: amount of bytes to copy
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int cs1, cs2, cfin;
+
+	for (cs1 = 0; src[cs1] != 0; cs1++)
+		;
+	for (cs2 = 0; dest[cs2] != 0; cs2++)
+		;
+	for (cfin = 0; cfin < n && cfin < cs1; cfin++)
+		dest[cs2 + cfin] = src[cfin];
+
+	return (dest);
+}
