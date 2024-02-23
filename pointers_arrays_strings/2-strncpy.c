4983dfa7acfc2eb35c5aec8b0d247ecdcca6781d
AguilarJ11 <8664@holbertonstudents.com>
2024-02-23 19:43:02 +0000

    commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cs1, csf;
+
+	for (cs1 = 0; src[cs1] != '\0'; cs1++)
+		;
+	for (csf = 0; csf < n; csf++)
+	{
+		if (csf <= cs1)
+			dest[csf] = src[csf];
+		else (csf > cs1)
+			dest[csf] = '\0';
+	}
+
+	return (dest);
+}
+
