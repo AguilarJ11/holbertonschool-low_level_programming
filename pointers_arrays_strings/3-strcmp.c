cfe8b7aedb3295a3edca5e9a2dffb1ea8f5d04c0
AguilarJ11 <8664@holbertonstudents.com>
2024-02-23 20:00:47 +0000

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int cs1, cs2;
 
-	for (cs1 = 0 s1[cs1] != '\0'; cs1++)
+	for (cs1 = 0; s1[cs1] != '\0'; cs1++)
 		;
-	for (cs2 = 0 s2[cs2] != '\0'; cs2++)
+	for (cs2 = 0; s2[cs2] != '\0'; cs2++)
 		;
 
 	if (cs1 > cs2)
