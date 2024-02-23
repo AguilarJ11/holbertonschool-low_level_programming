c6d35f7c403331cb7e1e517c30dcd793d8c8266a
AguilarJ11 <8664@holbertonstudents.com>
2024-02-23 17:31:19 +0000

    commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *print_array - stout
+ *@a: entry array
+ *@n: array lenght
+ */
+
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		if (c == n - 1)
+			printf("%d", a[c]);
+		else
+			printf("%d ", a[c]);
+	}
+	printf("\n");
+}
