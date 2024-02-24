7fb2016e7a5cab4b60e7269bb800336d230e2a67
AguilarJ11 <8664@holbertonstudents.com>
2024-02-24 00:04:23 +0000

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 	int cstr, c, mayus = 0;
 
 	for (cstr = 0; str[cstr] != '\0'; cstr++)
-	       ;
+		;
 
 	for (c = 0; c <= cstr; c++)
 	{
@@ -20,17 +20,18 @@ char *cap_string(char *str)
 			str[c] = str[c] - 32;
 			mayus = 1;
 		}
+			if (str[c] >= 65 && str[c] <= 90)
+				mayus = 1;
 
-		if (str[c] >= 65 && str[c] <= 90)
-		       mayus = 1;
-		if (str[c] >= 48 && str[c] <= 57)
-			mayus = 1;
-		if (str[c] == ' ' || str[c] == '	' || str[c] == '\n' ||
-		str[c] == '.' || str[c] == ';' || str[c] == '.' ||
-		str[c] == '!' || str[c] == '?' || str[c] == '"' ||
-		str[c] == '(' || str[c] == ')' || str[c] == '{' ||
-		str[c] == '}')
-			mayus = 0;
+			if (str[c] >= 48 && str[c] <= 57)
+				mayus = 1;
+
+			if (str[c] == ' ' || str[c] == '	' || str[c] == '\n' ||
+			str[c] == '.' || str[c] == ';' || str[c] == '.' ||
+			str[c] == '!' || str[c] == '?' || str[c] == '"' ||
+			str[c] == '(' || str[c] == ')' || str[c] == '{' ||
+			str[c] == '}')
+				mayus = 0;
 
 	}
 	return (str);
