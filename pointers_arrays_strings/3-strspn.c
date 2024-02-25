1f9cacf877669372cff1d51c8cd608a976e397d2
AguilarJ11 <8664@holbertonstudents.com>
2024-02-25 22:04:44 +0000

    commit

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@s: entry string
+ *@accept: entry substring
+ *Return: numbero of bytes in the initial segment of string
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int c;
+	unsigned int t = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == accept[c] && t == 0)
+			t++;
+		else
+			t = 0;
+	}
+
+	return (t);
+
+}
