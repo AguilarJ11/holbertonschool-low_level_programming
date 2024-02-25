15516743f4a0d39cbed031399f5aa1f2bd967608
AguilarJ11 <8664@holbertonstudents.com>
2024-02-25 21:08:26 +0000

    commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strchr - search for a character in a string
+ *@s: entry string
+ *@c: entry value to search
+ *Return: pointer of the first match or pointer a null value if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int cnt;
+
+	for (cnt = 0; s[cnt] != '\0'; cnt++)
+	{
+		if (s[cnt] == c)
+			return (&s[cnt]);
+	}
+	if (s[cnt] == '\0')
+		return (&s[cnt]);
+
+	return ('\0');
+
+}
+
