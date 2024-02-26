6755058db0caf3dafa35a31ffe7af71fcb3c7cf6
AguilarJ11 <8664@holbertonstudents.com>
2024-02-26 18:43:38 +0000

    commit

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strpbrk - search a for a match
+ *@s: entry string
+ *@accept: entry string
+ *return: return string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int c1, c2;
+
+	for (c1 = 0; s[c1] != '\0'; c1++)
+	{
+		for (c2 = 0; accept[c2] != '\0'; c2++)
+		{
+			if (s[c1] == accept[c2])
+				return (s + c1);
+		}
+	}
+	return ('\0');
+}
+
