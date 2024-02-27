973536305259db385c252c8ddb50c85bb10dd5ed
AguilarJ11 <8664@holbertonstudents.com>
2024-02-27 17:08:32 +0000

    commit

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 /**
- *
- *
+ *factorial - return a factorial of n
+ *@n: entry int
+ *Return: result of factorial of n
  */
 
 int factorial(int n)
 {
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
 
