78e8415910170f03407e4b7b962a34405e42505e
AguilarJ11 <8664@holbertonstudents.com>
2024-02-28 17:41:26 +0000

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,21 +9,23 @@
 
 int main(int argc, char *argv[])
 {
-	int c, isdig, sum;
+	int c1, c2, isdig, sum;
 
 	if (argc == 1)
 		printf("0\n");
 
-	for (c = 1; c < argc; c++)
+	for (c1 = 1; c1 < argc; c1++)
 	{
-		isdig = isdigit(argv[c]);
-		if (isdig == 1)
-			sum += atoi(argv[c]);
-		else
+		for (c2 = 0; c2 != '\0'; c2++)
 		{
-			printf("Error\n");
-			return (1);
+			isdig = isdigit(argv[c1][c2]);
+			if (isdig == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[c1]);
 	}
 	printf("%d", sum);
 	return (0);
