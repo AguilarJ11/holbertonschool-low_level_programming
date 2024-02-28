f3599a9176e74550dadfa5bc66e5e2365652c1d2
AguilarJ11 <8664@holbertonstudents.com>
2024-02-28 15:03:26 +0000

    commit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - multiplication 2 arguments
+ *@argc: n°of arg
+ *@argv: array of strings
+ *Return: 1 error 0 end
+ */
+
+int main(int argc, char *argv[])
+{
+	int c, m = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for(c = 1; c < argc; c++)
+		m *= atoi(argv[c]);
+
+	return (0);
+}
+
