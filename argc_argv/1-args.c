78e8017e6fe31ba9739c461870228dede1a2c4cd
AguilarJ11 <8664@holbertonstudents.com>
2024-02-28 14:13:53 +0000

    commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ *main - print the nÂ°of arguments
+ *@argc: nÂ of arguments
+ *@argv: array of arguments
+ *Return: end
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
