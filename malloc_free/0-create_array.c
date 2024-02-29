75024ea058ea3b936b3fc0ce47631c20107cd0b2
AguilarJ11 <8664@holbertonstudents.com>
2024-02-29 17:32:17 +0000

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -4,17 +4,22 @@
 /**
  *create_array - create an array with specific size
  *@size: entre size of array
- *@c: entry addess of array
+ *@c: entry char
+ *Return: array full with entry char
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int cont;
+
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
 	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
 
 	for (cont = 0; cont < size; cont++)
 		ptr[cont] = c;
