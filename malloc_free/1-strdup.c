e197bc9a734608d1b3bdfee456119451cbbd6a3b
AguilarJ11 <8664@holbertonstudents.com>
2024-02-29 18:18:10 +0000

    commit

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - duplicate a string and alocate in memoriy
+ *@str: entry pointer to string to duplicate
+ *Return: pointer to duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *p_array;
+	int c1, c2;
+
+	for (c1 = 0; str[c1] != '\0'; c1++)
+		;
+
+	p_array = malloc(c1);
+
+	if (p_array == 0)
+		return (NULL);
+
+	for (c2 = 0; c2 < c1; c2++)
+		p_array[c2] = str[c2];
+
+	return (p_array);
+}
