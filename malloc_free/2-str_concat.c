b08d9572077c948e7888e8952d62b9a09c9c52b0
AguilarJ11 <8664@holbertonstudents.com>
2024-03-01 13:22:04 +0000

    commit

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *str_concat - concatenate two strings y one array
+ *@s1: entry string
+ *@s2: e|ntry string
+ *Return: new array with the content of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p_array;
+	int c1, c2, ct = 0, f;
+
+	for (c1 = 0; s1[c1] != '\0'; c1++)
+		;
+	for (c2 = 0; s2[c2] != '\0'; c2++)
+		;
+	if ((s1 == 0) || (s2 == 0))
+		return (NULL);
+
+	ct = c1 + c2;
+
+	p_array = malloc(ct + sizeof(char));
+
+	if (p_array == 0)
+		return (NULL);
+
+	for (f = 0; f < ct; f++)
+	{
+		if (f <= c1 - 1)
+			p_array[f] = s1[f];
+		if (f <= c2)
+			p_array[f + c1 - 1] = s1[f];
+	}
+
+	return (p_array);
+}
+
+
+
+
