39494b6130affb05f33358218c2117a0a8e8185b
AguilarJ11 <8664@holbertonstudents.com>
2024-03-01 17:06:07 +0000

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,30 +10,30 @@
 int **alloc_grid(int width, int height)
 {
 	int c1, c2;
-	int *2_dim_array;
+	int **two_dim_array;
 
-	if (width <= 0) || (height <= 0)
+	if ((width <= 0) || (height <= 0))
 		return (NULL);
 
-	2_dim_array = malloc(height * sizeof(int));
+	two_dim_array = malloc(height * sizeof(int));
 
-	if (2_dim_array == 0)
+	if (two_dim_array == 0)
 		return(NULL);
 
 	for (c1 = 0; c1 < height; c1++)
 	{
-		2_dim_array [c1] = malloc(width * sizeof(int));
+		two_dim_array[c1] = malloc(width * sizeof(int));
 
-		if (2_dim_array[c1] == 0)
+		if (two_dim_array[c1] == 0)
 		{
-			free(2_dim_array);
+			free(two_dim_array);
 			return (NULL);
 		}
 
 		for (c2 = 0; c2 < width; c2++)
-			2_dim_array[c1][c2] == 0;
+			two_dim_array[c1][c2] = 0;
 	}
-	return (2_dim_array);
+	return (two_dim_array);
 }
 
 
