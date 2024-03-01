92d59316ac040cd19f440f0e2825fbaeff4fd70e
AguilarJ11 <8664@holbertonstudents.com>
2024-03-01 18:18:07 +0000

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  *alloc_grid - create a 2 dimension array fill with 0
@@ -10,7 +11,7 @@
 
 int **alloc_grid(int width, int height)
 {
-	int c1, c2;
+	int c1, c2, c3;
 	int **two_dim_array;
 
 	if ((width <= 0) || (height <= 0))
@@ -18,21 +19,27 @@ int **alloc_grid(int width, int height)
 
 	two_dim_array = malloc(height * sizeof(int*));
 
-	if (two_dim_array == 0)
+	if (two_dim_array == NULL)
 		return (NULL);
 
 	for (c1 = 0; c1 < height; c1++)
 	{
 		two_dim_array[c1] = malloc(width * sizeof(int));
 
-		if (two_dim_array[c1] == 0)
+		if (two_dim_array[c1] == NULL)
 		{
+			for (c3 =c1-1; c3 >= 0; c3--)
+				free(two_dim_array[c3]);
+
 			free(two_dim_array);
 			return (NULL);
 		}
 
 		for (c2 = 0; c2 < width; c2++)
+		{
+
 			two_dim_array[c1][c2] = 0;
+		}
 	}
 	return (two_dim_array);
 }
