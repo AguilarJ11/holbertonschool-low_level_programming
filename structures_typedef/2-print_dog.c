923534fd43e4bd67cb9fa34176e2b4f0e896b84d
AguilarJ11 <8664@holbertonstudents.com>
2024-03-04 22:09:30 +0000

    Commit

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *print_dog - print all values of dog
+ *@d: entry dog :W
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if ((*d).name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", (*d).name);
+		if ((*d).age == 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %1.f\n", (*d).age);
+		if ((*d).owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
+	}
+}
