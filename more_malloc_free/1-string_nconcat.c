41bd464149d20c657b8d7425743e209f9d248165
AguilarJ11 <8664@holbertonstudents.com>
2024-03-04 15:50:56 +0000

    commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,6 +12,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int c1,c2;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	if (n >= sizeof(s2))
 	{
 		p = malloc(sizeof(s1) + sizeof(s2));
