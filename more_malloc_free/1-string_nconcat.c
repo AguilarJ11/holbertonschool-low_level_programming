56fba128734f6c0470719de431c6d7676c59cd7c
AguilarJ11 <8664@holbertonstudents.com>
2024-03-04 15:48:16 +0000

    commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (c1 = 0; s1[c1] != '\0'; c1++)
 		p[c1] = s1[c1];
-	for (c2 = 0; s2[c2] != '\0' && c2 <= n; c2++)
+	for (c2 = 0; s2[c2] != '\0' && c2 < n; c2++)
 		p[c1 + c2] = s2[c2];
 
 	p[c1 + c2] = '\0';
