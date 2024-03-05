b2be6d2572bd0641cb36d14e22f5c659bc15cfb9
AguilarJ11 <8664@holbertonstudents.com>
2024-03-05 17:44:13 +0000

    Commit

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,64 @@
+#ifndef calc
+#define calc
+
+/**
+ *suma - suma
+ *@a: entry int
+ *@b: entry int
+ *Return: resultado a + b
+ */
+
+int suma(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *resta - resta
+ *@a: entry int
+ *@b: entry int
+ *Return: resultado a - b
+ */
+
+int resta(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *multi - multiplica
+ *@a: entry int
+ *@b: entry int
+ *Return: resultado a * b
+ */
+
+int multi(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *div - division
+ *@a: entry int
+ *@b: entry int
+ *Return: resultado a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *resto - resto de div
+ *@a: entry int
+ *@b: entry int
+ *Return: resto de div entre a y b
+ */
+
+int resto(int a, int b)
+{
+	return (a % b);
+}
+
+#endif
---
+++ function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main()
+{
+}
