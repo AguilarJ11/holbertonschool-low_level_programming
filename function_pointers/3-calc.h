3dbbecaf46b5f736eb88ebcd70bb5e6262da3788
AguilarJ11 <8664@holbertonstudents.com>
2024-03-06 13:03:05 +0000

    Commit

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -6,20 +6,20 @@
 /**
  *struct operator - estructura op mat
  *@op: operador mat + - / * %
- *f: puntero a funcion op
+ *@f: puntero a funcion op
  */
 
 typedef struct operator
 {
 	char *op;
 	int (*f)(int, int);
-}op_t;
+} op_t;
 
 int op_add(int, int);
 int op_sub(int, int);
 int op_mul(int, int);
 int op_div(int, int);
-int op_mod(int,int);
+int op_mod(int, int);
 int (*get_op_func(char *s))(int, int);
 
 #endif
