5163a214c4b5bac7b071f49cded4026719b1f312
AguilarJ11 <8664@holbertonstudents.com>
2024-03-25 17:37:18 +0000

    fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 	size_t cont = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -19,7 +19,6 @@ size_t print_list(const list_t *h)
 
 		h = h->next;
 		cont++;
-
 	}
 
 	return (cont);
