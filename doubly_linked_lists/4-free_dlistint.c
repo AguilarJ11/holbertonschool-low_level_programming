7704c108a648dc05215fd1168a3ab6821755aa1b
AguilarJ11 <8664@holbertonstudents.com>
2024-03-28 13:55:48 +0000

    f free_dlistint done

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *free_dlistint - free a node
+ *head: entry pointer of a node
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *dir;
+
+	while (head)
+	{
+		dir = head->next;
+		free(head);
+		head = dir;
+	}	
+}
