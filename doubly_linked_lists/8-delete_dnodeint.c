dd09b44532d07532e679c3a8d3fabeec779ce5b4
AguilarJ11 <8664@holbertonstudents.com>
2024-03-29 15:35:34 +0000

    f delete_dnodeint done

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -30,13 +30,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (cont = 0; cont < index && aux; cont++)
-	{
-		if (!aux)
-			return (-1);
-
+	for (cont = 0; cont < index && aux->next; cont++)
 		aux = aux->next;
-	}
+
+	if (cont < index)
+		return (-1);
 
 	aux->prev->next = aux->next;
 	aux->next->prev = aux->prev;
