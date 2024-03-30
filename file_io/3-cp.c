fe0e5dc2790bddd598a63bdcc4c86ef42e971d04
AguilarJ11 <8664@holbertonstudents.com>
2024-03-30 21:02:27 +0000

    look mom, first try!

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,45 +1,6 @@
 #include "main.h"
 
 /**
- *_strlen - len of a string
- *@str: entry string
- *Return: n°of char
- */
-
-int _strlen(char *str)
-{
-	int cont;
-
-	for (cont = 0; str[cont] != '\0'; cont++)
-		;
-
-	return (cont);
-}
-
-/**
- *_strdup - copy a string
- *@str: entry string to copy
- *Return: new copy of the entry string
- */
-
-char *_strdup(char *str)
-{
-	char *cstr;
-	int len;
-	int cont;
-
-	len = _strlen(str);
-	cstr = malloc(len + 1);
-	if (!cstr)
-		return (NULL);
-
-	for (cont = 0; str[cont] != '\0'; cont++)
-		cstr[cont] = str[cont];
-
-	return (cstr);
-}
-
-/**
  *main - copy a file
  *@argc: n° of arguments
  *@argv: content of the argumens
@@ -48,41 +9,68 @@ char *_strdup(char *str)
 
 int main(int argc, char *argv[])
 {
-	int fl, r, w;
+	int f1, f2, r, w;
 	char *text;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	if (!argv[2])
 	{
-		dprintf(STDERR_FILENO,"Error: Cant't read from file %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Cant't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fl = open(argv[2], O_RDWR);
-	if (fl == -1)
+	f1 = open(argv[1], O_RDWR);
+	if (f1 == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
+	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f2 == -1)
+	{
+		close(f1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
+	text = malloc(1024);
 	if (!text)
+	{
+		close(f1);
+		close(f2);
 		return (-1);
+	}
 
-	w = write(fl, text, _strlen(text));
+	r = read(f1, text, 1024);
+	if (r == -1)
+	{
+		close(f1);
+		close(f2);
+		free(text);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	w = write(f2, text, r);
 	if (w == -1)
 	{
-		close(fl);
+		close(f1);
+		close(f2);
 		free(text);
-		return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 
-	close(fl);
+	close(f1);
+	close(f2);
 	free(text);
+
+	return (0);
 }
 
