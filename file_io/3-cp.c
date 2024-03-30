3f46a01df1c6ed056bd3f34b8b19c12e73d7b1d2
AguilarJ11 <8664@holbertonstudents.com>
2024-03-30 22:11:10 +0000

    try some fixes, save progress

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,50 @@
 #include "main.h"
 
 /**
+ *a_errormen - print error mensj
+ *@fd: entry filedescriptor
+ *@error: entry error number
+ *@arg: entry argument value
+ */
+
+void a_errormen(int fd, int error, char *arg)
+{
+	switch (error)
+	{
+		case 98:
+			dprintf(fd, "Error: Cant't read from file %s\n", arg[1]);
+			exit(error);
+			break;
+		case 99:
+			dprintf(fd, "Error: Can't write to %s\n", arg[2]);
+			exit(error);
+			break;
+		default:
+			break;
+	}
+
+}
+
+/**
+ *_errormen - print error menj
+ *@fd: entry filedescriptor
+ *@error: error number
+ */
+
+void _errormen(int fd, int error)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(fd, "Usage: cp file_from file_to\n");
+			exit(error);
+			break;
+		case 100:
+			dprintf(fd, "Error: Can't close %i\n", fd);
+			exit(error);
+}
+
+/**
  *main - copy a file
  *@argc: n° of arguments
  *@argv: content of the argumens
@@ -10,20 +54,18 @@
 int main(int argc, char *argv[])
 {
 	int f1, f2, r, w;
-	char *text;
+	char *text[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	if (!argv[2])
 	{
 		dprintf(STDERR_FILENO, "Error: Cant't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	f1 = open(argv[1], O_RDWR);
 	if (f1 == -1)
 	{
@@ -38,39 +80,24 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	text = malloc(1024);
-	if (!text)
-	{
-		close(f1);
-		close(f2);
-		return (-1);
-	}
-
 	r = read(f1, text, 1024);
 	if (r == -1)
 	{
 		close(f1);
 		close(f2);
-		free(text);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	w = write(f2, text, r);
 	if (w == -1)
 	{
 		close(f1);
 		close(f2);
-		free(text);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	close(f1);
 	close(f2);
-	free(text);
-
 	return (0);
 }
 
